b3f0c963711763155bd33d8bed816b4525e4208f
sivabala142 <sivabala142@gmail.com>
2022-04-29 15:05:21 +0530

    Api response-getallmemo

--- src/App.tsx
+++ src/App.tsx
@@ -24,9 +24,11 @@ console.log("API_BASE_URL",API_BASE_URL);
 
 
 const authLink = setContext((_, { headers }) => {
-    const token = cookies.get('access_token_weavit');
+    // const token = cookies.get('access_token_weavit');
+    const token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImM2NzNkM2M5NDdhZWIxOGI2NGU1OGUzZWRlMzI1NWZiZjU3NTI4NWIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vd2Vhdml0LWludGVncmF0aW9uLTEiLCJhdWQiOiJ3ZWF2aXQtaW50ZWdyYXRpb24tMSIsImF1dGhfdGltZSI6MTY1MTIyMzMwMiwidXNlcl9pZCI6Imdvb2dsZToxMDkzMzk4ODgxOTU5MzU1MjY4NjgiLCJzdWIiOiJnb29nbGU6MTA5MzM5ODg4MTk1OTM1NTI2ODY4IiwiaWF0IjoxNjUxMjIzMzAyLCJleHAiOjE2NTEyMjY5MDIsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnt9LCJzaWduX2luX3Byb3ZpZGVyIjoiY3VzdG9tIn19.A-c-CMwTho_ob-zdKKVyHPARcxm_QJ2vlQqNePq-VwBlDNuK-zzwz-uarZFILbICuxFwe8-1DsP62gYGKvxH2FsdWKR-D0QsqXDLfpfFRqrwg9LQ2wwJudjLNDadLtnOSJM-xPWBNDmu97J4K6AAkDXWZ3_o8c5CvdYt7kyY7XZf7MNoW7tMOWk_rj_PpRb8ftK9r5l7BfoPn7V9kr5zRfOZIqg5RiHgBm4w5MZ5DzSONVtWWTxGvkFAAvvuR4KiU3KDPddKAV8U2rqa0Q36B2YcjvvuvriVlhPF2khxJMKBwI-zZEYH-b2WuQr4MXvcrNBxHzyZiNzpkGkaIkNdNg';
     // return the headers to the context so httpLink can read them
-    return {
+    
+    return { 
         headers: {
             ...headers,
             authorization: token ? `Bearer ${token}` : '',
@@ -37,6 +39,7 @@ const authLink = setContext((_, { headers }) => {
 const client = new ApolloClient({
     link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
+    
 });
 
 function App() {
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState , useEffect,useRef } from 'react';
 import { useGlobalContext } from '../../hooks/GlobalContext';
 // import { WIButton } from '@weavit/weavit-material-ui';
-import { useQuery, gql } from "@apollo/client";
-
+import { useQuery, gql, useMutation ,ApolloClient, createHttpLink, DefaultOptions, from, InMemoryCache, LazyQueryHookOptions, OperationVariables, QueryTuple, TypedDocumentNode, useLazyQuery  } from "@apollo/client";
+import { DocumentNode, GraphQLError } from 'graphql';
+import { GET_ALL_MEMO } from '../../services/queries';
+import { IGetAllMemoQueryResult, IGetAllMemoQueryVariables } from '../../graphql-models';
 export const Dashboard = () => {
+    const hasNext = useRef<boolean>(false);
     const { user } = useGlobalContext();
-    const GetAllMemos = gql`
-    {
-        getAllMemo(currentPage: 1, pageSize: 1){
-    count
-  }
-      }
-  `;
-    const { data, loading, error } = useQuery(GetAllMemos);
-    console.log(data, error);
+    const [queryMemos, { error, data, loading }] = useLazyQueryNoCache<
+    IGetAllMemoQueryResult,
+    IGetAllMemoQueryVariables
+  >(GET_ALL_MEMO);
 
+console.log(error,"error");
+console.log(data,"data");
+
+  const fetchPage = () => {
+    hasNext.current = false;
+    // fetchTimeline();
+  };
+
+  useEffect(() => {
+    // fetchPage();
+    fetchTimeline();
+  }, []);
+
+    const fetchTimeline = async (addition?: Partial<IGetAllMemoQueryVariables>) => {
+        const variables: IGetAllMemoQueryVariables = {
+          pageSize: PAGE_SIZE,
+          skipToken: 0,
+          ...(addition ?? {}),
+        };
+        queryMemos({ variables });
+      };
     return (
-        <React.Fragment>
+        <React.Fragment> 
+                
             {/* <WIButton
                 label={'Weavit Button'}
                 labelStyle={{
@@ -30,9 +50,19 @@ export const Dashboard = () => {
                 onPress={async () => console.log('Pressed')}
             /> */}
             <span>
-                {' '}
-                User ID / Name: {user?.userId} / {user?.names?.firstName} {user?.names?.lastName}
-            </span>
+               {' '}
+             User ID / Name: {user?.userId} / {user?.names?.firstName} {user?.names?.lastName}
+           </span>
         </React.Fragment>
     );
 };
+
+export function useLazyQueryNoCache<TData = any, TVariables = OperationVariables>(query: DocumentNode | TypedDocumentNode<TData, TVariables>, options?: LazyQueryHookOptions<TData, TVariables> | undefined): QueryTuple<TData, TVariables> {
+    return useLazyQuery<TData, TVariables>(query, {
+      fetchPolicy: 'no-cache',
+      nextFetchPolicy: 'no-cache',
+      ...(options ?? {}),
+    });
+  }
+
+  export const PAGE_SIZE = 50;
